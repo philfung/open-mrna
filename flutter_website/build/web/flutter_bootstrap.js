3bf16b020a9d3e937294a07e2833f6acc6adf2a1
Philip Fung <no@one>
2026-03-25 08:10:12 -0700

    build

--- flutter_website/build/web/flutter_bootstrap.js
+++ flutter_website/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2442966052" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2138586090" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
